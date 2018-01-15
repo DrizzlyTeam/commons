102dd017cf92770aad0cc6c3e2fb92e26eaaaa46
Valentin <contact@vfrz.fr>
2018-01-15 14:48:36 +0100

    First commit

---
+++ Drizzly.Commons/JsonResultExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drizzly.Commons
+{
+    public static class JsonResultExtensions
+    {
+        public static JsonResult CreateEmptyResult(ResponseStatus status = ResponseStatus.Success, string message = "")
+        {
+            return new JsonResult(new
+            {
+                Status = status.ToString().ToLower(),
+                StatusMessage = message,
+                Data = ""
+            });
+        }
+
+        public static JsonResult ToUnifiedResult(this JsonResult result, ResponseStatus status = ResponseStatus.Success,
+            string message = "")
+        {
+            return new JsonResult(new
+            {
+                Status = status.ToString().ToLower(),
+                StatusMessage = message,
+                Data = result.Value
+            });
+        }
+    }
+
+    public enum ResponseStatus
+    {
+        Success,
+        Error
+    }
+}
