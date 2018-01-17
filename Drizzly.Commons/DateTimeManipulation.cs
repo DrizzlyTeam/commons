52875f8f2aaf57ea3965e50a75b6e6a8e0138291
Alvin FREY <alvin.frey@protonmail.ch>
2018-01-17 21:30:59 +0100

    Adding DateTimeManipulation class to Drizzly Commons library (Unix Timestamp to DateTime and DateTime to UnixTimestamp)

---
+++ Drizzly.Commons/DateTimeManipulation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Drizzly.Commons
+{
+    public static class DateTimeManipulation
+    {
+        public static DateTime TimestampToDateTime(this double unixTimeStamp)
+        {
+            
+            DateTime datetime = new DateTime(1970,1,1,0,0,0,0, DateTimeKind.Utc);
+            datetime = datetime.AddSeconds(unixTimeStamp).ToLocalTime();
+            
+            return datetime;
+            
+        }
+
+        public static double DateTimeToTimestamp(DateTime datetime)
+        {
+            return ((DateTimeOffset) datetime).ToUnixTimeSeconds();
+        }
+
+    }
+}
